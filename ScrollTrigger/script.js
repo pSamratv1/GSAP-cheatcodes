a7016f192485880259c20960c46ffb62b6bfd82d
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-17 21:24:03 +0545

    Experience Text Slider added

---
+++ ScrollTrigger/script.js
@@ -0,0 +1,28 @@
+// gsap.from("#page1 #box", {
+//   scale: 0,
+//   duration: 2,
+//   delay: 1,
+//   rotate: 360,
+// });
+// gsap.from("#page2 #box", {
+//   scale: 0,
+//   duration: 2,
+//   rotate: 360,
+//   scrollTrigger: {
+//     trigger: "#page2 #box",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 50%",
+//   },
+// });
+
+gsap.to("#page2 h1", {
+  transform: "translate(-188%)",
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller: "body",
+    markers: true,
+    start: "top 0%",
+  },
+});
