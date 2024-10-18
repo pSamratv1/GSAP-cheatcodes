1acfa2094027efceb88c9d41d722a9bba4147fc1
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-19 02:11:05 +0545

    Cursor Tracker Completed

--- CursorTracking/script.js
+++ CursorTracking/script.js
@@ -1,7 +1,7 @@
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
 var ball = document.querySelector("#ball");
-var imageDiv
+var imageDiv = document.querySelector("#image");
 
 main.addEventListener("mousemove", function (dets) {
   gsap.to(cursor, {
@@ -10,6 +10,20 @@ main.addEventListener("mousemove", function (dets) {
     duration: 1,
 
     ease: "back.out",
+  });
+});
+// 9867675004
+imageDiv.addEventListener("mouseenter", function () {
+  cursor.innerHTML = "View More";
+  gsap.to(cursor, {
+    scale: 3.5,
+  });
+});
+imageDiv.addEventListener("mouseleave", function () {
+  cursor.innerHTML = "";
+
+  gsap.to(cursor, {
+    scale: 1,
   });
 });
 ball.addEventListener("mousemove", function (dets) {
