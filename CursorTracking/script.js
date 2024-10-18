1d3c324d364cc76d11d3ad9bc449818d61471b40
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-19 01:18:05 +0545

    New ball drop animation added

--- CursorTracking/script.js
+++ CursorTracking/script.js
@@ -1,9 +1,21 @@
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
+var ball = document.querySelector("#ball");
+var imageDiv
 
 main.addEventListener("mousemove", function (dets) {
   gsap.to(cursor, {
     x: dets.x,
     y: dets.y,
+    duration: 1,
+
+    ease: "back.out",
+  });
+});
+ball.addEventListener("mousemove", function (dets) {
+  gsap.to(ball, {
+    y: 510,
+    duration: 1,
+    ease: "bounce.out",
   });
 });
