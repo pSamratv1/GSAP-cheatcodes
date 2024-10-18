0fc1c45b26bd6f44f22316650cabaa3f68a99372
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-18 14:40:12 +0545

    SVG path string animation completed

--- SVGLineAnimation/script.js
+++ SVGLineAnimation/script.js
@@ -1,17 +1,20 @@
-var path = `M 10 200 Q 818 200 1626 200`;
-const initialPath = `M 10 200 Q 818 200 1626 200`;
+var path = `M 100 200 Q 818 200 1536 200`;
+const initialPath = `M 100 200 Q 818 200 1536 200`;
 
 var string = document.querySelector("#string");
 
 string.addEventListener("mousemove", function (dets) {
-  path = `M 10 200 Q ${dets.x} ${dets.y} 1626 200`;
+  path = `M 100 200 Q ${dets.x} ${dets.y} 1536 200`;
   gsap.to("#string svg path", {
     attr: { d: path },
+    duration: 0.2,
   });
 });
 
 string.addEventListener("mouseleave", function () {
   gsap.to("svg path", {
     attr: { d: initialPath },
+    duration: 0.7,
+    ease: "elastic.out(1,0.2)",
   });
 });
