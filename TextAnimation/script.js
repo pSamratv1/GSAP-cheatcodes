aa9f72cb332dd5a98d8375a578696e4847f26e46
CODE-SAMRAT <paharisamrat@gmail.com>
2024-10-21 02:22:47 +0545

    Text Animations Completed

--- TextAnimation/script.js
+++ TextAnimation/script.js
@@ -3,19 +3,33 @@ function brokenText() {
   var h1Text = h1.textContent;
 
   var splitedText = h1Text.split("");
+  var stringLength = splitedText.length;
+
+  var halfLength = Math.floor(stringLength / 2);
 
   var currentText = "";
-  splitedText.forEach((ele) => {
-    currentText += `<span class="a">${ele}</span>`;
+  splitedText.forEach(function (ele, idx) {
+    if (idx < halfLength) {
+      currentText += `<span class="a">${ele}</span>`;
+    } else currentText += `<span class="b">${ele}</span>`;
   });
 
   h1.innerHTML = currentText;
 }
 brokenText();
-gsap.from("h1 span", {
+gsap.from("h1 .a", {
   y: 200,
   opacity: 0,
   duration: 0.8,
   delay: 0.5,
   stagger: 0.15,
+  scrub: 2,
+});
+gsap.from("h1 .b", {
+  y: 200,
+  opacity: 0,
+  duration: 0.8,
+  delay: 0.5,
+  stagger: -0.15,
+  scrub: 2,
 });
